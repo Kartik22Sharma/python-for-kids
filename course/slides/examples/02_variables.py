821d7cd318042fa2972f428389960eed80fbacd7
Kartik22Sharma <100220163+Kartik22Sharma@users.noreply.github.com>
2026-04-25 02:03:51 +0530

    Add example of variable usage in Python

---
+++ course/slides/examples/02_variables.py
@@ -0,0 +1,10 @@
+# 02 - Variables
+# Making magic boxes that hold stuff!
+
+name = "Riya"
+age = 7
+favorite_color = "pink"
+
+print("My name is", name)
+print("I am", age, "years old")
+print("My favorite color is", favorite_color)
